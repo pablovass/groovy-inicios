d598f40fe7dce7026e1ccb022ba12eda7c89bce6
pablovass <faktory.fm@live.com>
2024-05-01 21:42:46 -0300

    guardar proyecto

---
+++ tutorial00/src/main/groovy/com/pablovass/Main.groovy
@@ -0,0 +1,10 @@
+package com.pablovass
+
+static void main(String[] args) {
+
+    Person person= new Person()
+    person.setName('Pablo')
+    person.setAge(37)
+
+    println("NAME: ${person.getName()} AGE: ${person.getAge()}")
+}
---
+++ tutorial00/src/main/groovy/com/pablovass/Person.groovy
@@ -0,0 +1,17 @@
+package com.pablovass
+
+class Person {
+    String name
+    int age
+
+    String getName() {
+        return name
+    }
+
+    void setName(String name) {
+        this.name = name
+    }
+}
+
+
+
